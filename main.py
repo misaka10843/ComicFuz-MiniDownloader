8a1bb482b90bf250aedafee961e444ce948e8612
misaka10843 <misaka10843@outlook.jp>
2024-07-15 19:47:48 +0800

    update

--- main.py
+++ main.py
@@ -1,59 +1,61 @@
-#!/usr/bin/env python3
-# todo 1. 将urllib更换成requests
-# todo 2. 将所有输出切换为logging
-# todo 3. 预留参数接口
-# todo 4. 调用返回状态以及压缩包路径
+import getpass
+import json
 import os
 import re
 import time
+from os.path import basename
+from queue import Queue
+from threading import Thread
+from zipfile import ZipFile
+
+import requests
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from google.protobuf import json_format
+from rich import print
+from rich.console import Console
+from rich.progress import track
 
 import fuz_pb2
-import json
-import py7zr
-import getpass
-import logging
-from urllib.request import Request, ProxyHandler, build_opener
-from google.protobuf import json_format
-from threading import Thread
-from queue import Queue
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+console = Console()
 
 COOKIE = "is_logged_in=true; fuz_session_key="
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" \
-             " AppleWebKit/537.36 (KHTML, like Gecko)" \
-             " Chrome/96.0.4664.55" \
-             " Safari/537.36" \
-             " Edg/96.0.1054.34"
+USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 "
+              "Safari/537.36 Edg/126.0.0.0")
 
 API_HOST = "https://api.comic-fuz.com"
 IMG_HOST = "https://img.comic-fuz.com"
 TABLE = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
 T_MAP = {s: i for i, s in enumerate(TABLE)}
+PROXY = {}
 
-urlopen = build_opener()
 
-
-def main(proxy, output_dir, token_file, user_email, password, magazine):
-    global urlopen
-    n_jobs = 16
+def main(output_dir, user_email, password, token_file, proxy, magazine, compress):
+    global PROXY
     # 添加代理
     if proxy:
-        proxy_handler = ProxyHandler(
-            {'http': f'http://{proxy}', 'https': f'http://{proxy}'})
-        urlopen = build_opener(proxy_handler)
-    else:
-        urlopen = build_opener()
+        PROXY = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
     print(
         "[white]= [#f9487d]ComicFuz-Extractor [#00BFFF]made with [red]:red_heart-emoji: [#00BFFF]by EnkanRec Repaired "
         "[white]and [#00BFFF]Modified and repair by misaka10843[white]=")
     os.makedirs(output_dir, exist_ok=True)
 
     token = get_session(token_file, user_email, password)
-    que = Queue(n_jobs)
+    get_store_index(token)
+    que = Queue(4)
     Thread(target=worker, args=(que,), daemon=True).start()
-    downloaded_path = down_magazine(output_dir, magazine, token, que)
-    compression(downloaded_path, output_dir)
-    logging.debug("Done.")
+    downloaded_path = ""
+    if ',' in magazine:
+        string_list = magazine.split(',')
+        for item in string_list:
+            downloaded_path = down_magazine(output_dir, int(item), token, que)
+            print(f"[bold green]正在等待10s后继续下载")
+            time.sleep(10)
+    else:
+        downloaded_path = down_magazine(output_dir, int(magazine), token, que)
+    if compress:
+        compression(downloaded_path, output_dir)
+    print(f"[bold green]全部任务已完成！")
 
 
 def sign(email: str, password: str) -> str:
@@ -62,19 +64,19 @@ def sign(email: str, password: str) -> str:
     body.email = email
     body.password = password
     url = API_HOST + "/v1/sign_in"
-    req = Request(url, body.SerializeToString(), method="POST")
     try:
-        with urlopen.open(req) as r:
-            res = fuz_pb2.SignInResponse()
-            res.ParseFromString(r.read())
-            if not res.success:
-                logging.error("Login failed")
-                exit(1)
-            for header in r.headers:
-                m = re.match(r'fuz_session_key=(\w+)(;.*)?', r.headers[header])
-                if m:
-                    return m.group(1)
-    except Exception as e:
+        response = requests.post(url, data=body.SerializeToString(), proxies=PROXY)
+        res = fuz_pb2.SignInResponse()
+        res.ParseFromString(response.content)
+        if not res.success:
+            print(f"[bold red]登录失败,请检查您的账号密码是否准确无误")
+            exit(1)
+        for header in response.headers:
+            m = re.match(r'fuz_session_key=(\w+)(;.*)?', response.headers[header])
+            if m:
+                return m.group(1)
+    except Exception:
+        console.print_exception(show_locals=True)
         exit()
 
 
@@ -84,40 +86,43 @@ def check_sign(token: str) -> bool:
         "user-agent": USER_AGENT,
         "cookie": COOKIE + token
     }
-    req = Request(url, headers=headers, method="POST")
     try:
-        with urlopen.open(req) as r:
-            res = fuz_pb2.WebMypageResponse()
-            res.ParseFromString(r.read())
-            if res.mailAddress:
-                print(f"[#FFB6C1]Login as: {res.mailAddress}")
-                return True
-            return False
-    except Exception as e:
+        response = requests.post(url, headers=headers, proxies=PROXY)
+        res = fuz_pb2.WebMypageResponse()
+        res.ParseFromString(response.content)
+        if res.mailAddress:
+            print(f"[#FFB6C1]Login as: {res.mailAddress}")
+            return True
+        return False
+    except Exception:
+        console.print_exception(show_locals=True)
         exit()
 
 
 def get_session(file: str, user: str, pwd: str) -> str:
     if not file and not user:
-        logging.info("Disable login, get only free part.")
+        print(f"[bold yellow]取消登录,将只能获取免费内容")
         return ""
     if file and os.path.exists(file):
         with open(file) as f:
             token = f.read().strip()
         if check_sign(token):
             return token
-        logging.debug("Get failed, try signing")
+        print(f"[bold yellow]获取失败,请重新登录尝试")
     user = user if user else input("您的邮箱: ")
     pwd = pwd if pwd else getpass.getpass("您的密码: ")
-    token = sign(user, pwd)
+
     try:
-        with open(file, "w") as f:
-            f.write(token)
-        print(f"[bold green]您的token已经存放到{file}中，请妥善保管")
+        token = sign(user, pwd)
+        if file:
+            with open(file, "w") as f:
+                f.write(token)
+            print(f"[bold green]您的token已经存放到{file}中,请妥善保管")
         return token
-    except Exception as e:
-        print(
-            "\n[bold yellow]自动获取token出错！\n请如果输出fuz_session_key类似的内容，请从fuz_session_key=开始复制("
+    except Exception:
+        console.print_exception(show_locals=True)
+        console.print(
+            "\n[bold yellow]自动获取token出错！\n请如果输出fuz_session_key类似的内容,请从fuz_session_key=开始复制("
             "不包含fuz_session_key=)到第一个分号(;)\n然后存入-t指定的文件中")
         exit()
 
@@ -129,18 +134,18 @@ def b64_to_10(s: str) -> int:
     return i
 
 
-def get_index(path: str, body: str, token: str) -> str:
+def get_index(path: str, body: str, token: str) -> bytes:
     url = API_HOST + path
     headers = {"user-agent": USER_AGENT}
     if token:
         headers["cookie"] = COOKIE + token
-    req = Request(url, body, headers, method="POST")
-    try:
-        with urlopen.open(req) as r:
-            return r.read()
-    except Exception as e:
-        print("\n[bold red]获取相关信息出错！\n请检查ID参数是否正确！\n或者稍后重试")
-        exit()
+
+    response = requests.post(url, data=body, headers=headers, proxies=PROXY)
+
+    if response.status_code != 200:
+        raise Exception("获取相关信息出错！请检查ID参数是否正确！或者稍后重试")
+
+    return response.content
 
 
 def get_magazine_index(magazine_id: int, token: str) -> fuz_pb2.MagazineViewer2Response:
@@ -155,40 +160,48 @@ def get_magazine_index(magazine_id: int, token: str) -> fuz_pb2.MagazineViewer2R
     return index
 
 
+def get_store_index(token: str) -> fuz_pb2.BookStorePage:
+    body = fuz_pb2.BookStorePageRequest()
+    body.deviceInfo.deviceType = fuz_pb2.DeviceInfo.DeviceType.BROWSER
+
+    res = get_index("/v1/store_3", body.SerializeToString(), token)
+    index = fuz_pb2.BookStorePage()
+    index.ParseFromString(res)
+    mid = str(index.info.nested_message3.details.id)
+    date = str(index.info.nested_message3.details.updateDate3)
+    name = str(index.info.nested_message3.details.magazineName)
+    print(mid, date, name)
+
+
 def download(save_dir: str, image: fuz_pb2.ViewerPage.Image, overwrite=False):
     if not image.imageUrl:
-        logging.debug("Not an image: %s", image)
+        print(f"[blue]无法获取图片链接,返回内容如下: {image}")
         return
     name = re.match(r'.*/([0-9a-zA-Z_-]+)\.(\w+)\.enc\?.*', image.imageUrl)
     if not name or not name.group(1):
-        logging.debug("Can't gass filename: %s", image)
+        print(f"[blue]无法检测文件名,返回内容如下: {image}")
         return
     name_num = "%03d" % b64_to_10(name.group(1))
     name = f"{save_dir}{name_num}.{name.group(2)}"
     if not overwrite and os.path.exists(name):
-        logging.debug("Exists, continue: %s", name)
+        print(f"图片已有,将跳过此图片,返回内容如下: {name}")
         return
     try:
-        with urlopen.open(IMG_HOST + image.imageUrl) as r:
-            data = r.read()
-    except Exception as e:
+        data = requests.post(IMG_HOST + image.imageUrl, proxies=PROXY).content
+    except Exception:
         time.sleep(5)
-        with urlopen.open(IMG_HOST + image.imageUrl) as r:
-            data = r.read()
+        data = requests.post(IMG_HOST + image.imageUrl, proxies=PROXY).content
     key = bytes.fromhex(image.encryptionKey)
     iv = bytes.fromhex(image.iv)
     decryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).decryptor()
     out = decryptor.update(data) + decryptor.finalize()
     with open(name, "wb") as f:
         f.write(out)
-    # os.system(f"curl -s \"{IMG_HOST}{image.imageUrl}\" | openssl aes-256-cbc -d -K {image.encryptionKey} -iv {
-    # image.iv} -in - -out {name}")
-    logging.debug("Downloaded: %s", name)
 
 
 def down_pages(
         save_dir: str,
-        data,  # : fuz_pb2.BookViewer2Response | fuz_pb2.MagazineViewer2Response | fuz_pb2.MangaViewerResponse,
+        data,
         que: Queue,
         book_name: str
 ):
@@ -201,7 +214,7 @@ def down_pages(
 
     # downloadThumb(save_dir, data.bookIssue.thumbnailUrl)
 
-    for page in data.pages:
+    for page in track(data.pages, description=f"[bold yellow]正在下载:{book_name}[/]"):
         t = Thread(target=download, name=page.image.imageUrl,
                    args=(save_dir, page.image))
         t.start()
@@ -222,10 +235,11 @@ def down_magazine(out_dir: str, magazine_id: int, token: str, que: Queue):
     elif magazine_name == 'まんがタイムきららフォワード':
         magazine_name = "Forward"
     down_pages(
-        f"{out_dir}/{magazine_name}{has_numbers(str(magazine.magazineIssue.magazineIssueName))}/", magazine, que,
+        f"{out_dir}/{magazine_name}/{magazine_name}{has_numbers(str(magazine.magazineIssue.magazineIssueName))}/",
+        magazine, que,
         f"[{magazine_name}]{magazine.magazineIssue.magazineIssueName}[/]")
     print(
-        f"[bold green]{has_numbers(str(magazine.magazineIssue.magazineIssueName))}下载完成！如果下载时遇见报错，请重新运行一下命令即可")
+        f"[bold green]{has_numbers(str(magazine.magazineIssue.magazineIssueName))}下载完成！如果下载时遇见报错,请重新运行一下命令即可")
     return f"{magazine_name}{has_numbers(str(magazine.magazineIssue.magazineIssueName))}"
 
 
@@ -236,9 +250,17 @@ def has_numbers(chat):
 
 def compression(download_dir: str, out_dir: str):
     print("[bold yellow]正在进行压缩中...")
-    with py7zr.SevenZipFile(f'{out_dir}/{download_dir}_og.7z', 'w') as archive:
-        archive.writeall(f"{out_dir}/{download_dir}_og")
-    print(f"[bold green]已经将原图打包压缩到{out_dir}/{download_dir}_og.7z")
+    with console.status(f"[bold yellow]正在将{download_dir}压缩成zip中"):
+        file_paths = []
+        for root, _, files in os.walk(f'{out_dir}/{download_dir}'):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+                break  # 仅遍历当前目录,不进入子目录
+        with ZipFile(f'{out_dir}/{download_dir}.zip', 'w') as z:
+            for f in files:
+                z.write(f, arcname=basename(f))
+    print(f"[bold green]已经将图片打包压缩到{out_dir}/{download_dir}.zip")
 
 
 def worker(que: Queue):
@@ -247,5 +269,7 @@ def worker(que: Queue):
         item = que.get()
         count += 1
         item.join()
-        # logging.debug("[%d] ok.", count)
         que.task_done()
+
+
+main()
